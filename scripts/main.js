905a331cd006ba17b808dd7ddb1703b85a99b288
rafiafandi <108367042+rafiafandi@users.noreply.github.com>
2025-01-06 13:54:49 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+// Toggle and Responsive
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
+// Clear form before unload
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("Form")) {
+    form.reset();
+  }
+};
